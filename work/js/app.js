00512814e8f63e7e5c1577b6019d3adef2359f07
sota1223 <sutkym0937@icloud.com>
2025-04-12 18:06:38 +0900

    コミット

---
+++ work/js/app.js
@@ -0,0 +1,18 @@
+document.addEventListener(`DOMContentLoaded`, () => {
+  fetch(`data/product.json`)
+    .then(response => response.json())
+    .then(data => {
+      data.forEach(product => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+        <td>${product.id}</td>
+        <td>${product.name}</td>
+        <td>${product.price}</td>
+        <td>${product.description}</td>
+        `;
+        document.getElementById('productTable').appendChild(row);
+      });
+    })
+    .catch(error => console.error('エラー;', error));
+}
+)
